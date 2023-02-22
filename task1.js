86d86abae73fcd50812ac0e596c2b4e81c6306b5
YauheniKastsevich <evgecha123@gmail.com>
2023-02-22 18:18:27 +0300

    3

--- task1.js
+++ task1.js
@@ -1,1 +1,9 @@
-git@github.com:YauheniKastsevich/codewars.git
+function numberToPower(number, power){
+    console.info(Math.log2(1024));
+    let result = 1;
+    for(let i=1; i <= power; i++){
+      result = number * result;
+    }
+    return result  
+  }
+  
